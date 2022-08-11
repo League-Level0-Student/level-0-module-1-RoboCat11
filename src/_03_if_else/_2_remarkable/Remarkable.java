b9993de4df472631733db0683e7f7400f7b360a3
league <league@imac-34.attlocal.net>
2022-08-11 16:59:17 -0700

    Today I did Remarkable and ALMOST finished Secret Message Box.

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,13 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+			  JOptionPane.showInputDialog("What is your name?");
+			  JOptionPane.showInputDialog("Alo is very good at origami.");
+			  JOptionPane.showInputDialog("Mana is very good at playing the Nintendo Switch.");
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+			  JOptionPane.showInputDialog("Please enter a name");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-
+			  JOptionPane.showInputDialog("Something remarkable about my sister is that she's good ar origami.");
 	}
 }
 
