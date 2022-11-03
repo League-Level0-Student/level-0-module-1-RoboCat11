5b6b6235d50b57909ef3e10278170fec6ffbc6bc
league <league@imac-14.attlocal.net>
2022-11-03 16:57:44 -0700

    Today finished The Riddler, finished Robot in Space, and started on Zombie eyes.

--- src/_04_int/_1_riddler/TheRiddler.java
+++ src/_04_int/_1_riddler/TheRiddler.java
@@ -18,7 +18,6 @@ public class TheRiddler {
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
 			  JOptionPane.showInputDialog("Correct!");
 		// 5. Otherwise, say "wrong" and tell them the answer
-
 		// 6. Add some more riddles
 			  JOptionPane.showInputDialog("What goes down but never comes back up?");
 			  JOptionPane.showInputDialog("Correct!");
@@ -34,10 +33,10 @@ public class TheRiddler {
 			  JOptionPane.showInputDialog("Correct!");
 			  JOptionPane.showInputDialog("What is the beginning of eternity, the end of time and space, and the beginning of every end?");
 			  JOptionPane.showInputDialog("Wrong. The answer was the letter e.");
-			  JOptionPane.showInputDialog("If you drop me I could break. But if you smile I will still smile back. What am I?");
+			  JOptionPane.showInputDialog("If you drop me I could crack. But if you smile I will still smile back. What am I?");
 			  JOptionPane.showInputDialog("Wrong. You were supposed to say mirror!");
 			  JOptionPane.showInputDialog("Mary's father has 5 daughters: Nana, Nene, Nini, Nono... who is the last daughter?");
-			  JOptionPane.showInputDialog("Wrong.Mary is the last daughter!");
+			  JOptionPane.showInputDialog("Wrong. Mary is the last daughter!");
 		// 2. Make a pop up to show the score.
 		      score++;
 	}
