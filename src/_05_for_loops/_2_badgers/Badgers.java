079d08b78bab21966fea109389f7027f7ec701f2
league <league@imac-18.attlocal.net>
2022-11-19 11:26:14 -0800

    Today I finished Badgers, did Eva Bananas, and started on Banana Split.

---
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -0,0 +1,21 @@
+package _05_for_loops._2_badgers;
+
+public class Badgers {
+
+	public static void main(String[] args) {
+	for(int k=0; k<2; k++) {
+	for(int i=0; i<12; i++) {
+	System.out.println("Badger");
+	}
+	for(int i=0; i<2; i++) {
+	System.out.println("Mushroom");
+	}
+	}
+	{
+	System.out.println("A Snake!");
+}
+
+}
+}
+
+
